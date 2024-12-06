d17d4363362e771f342ac57761271e316af45cc5
Fer_ <fg.roglia@gmail.com>
2024-12-06 21:03:36 +0100

    Ejercicio_2_Nivel_1_Finalizado

--- src/nivel_1/ejercicio2/GenericMethods.java
+++ src/nivel_1/ejercicio2/GenericMethods.java
@@ -2,10 +2,10 @@ package nivel_1.ejercicio2;
 
 public class GenericMethods {
 
-    public static <T1, T2, T3> void printArguments(T1 arg1, T2 arg2, T3 arg3) {
-        System.out.println("Arguments 1: " + arg1);
-        System.out.println("Arguments 2: " + arg2);
-        System.out.println("Arguments 3: " + arg3);
+    public static <T1, T2, T3> void printArguments(T1 argument1, T2 argument2, T3 argument3) {
+        System.out.println("Arguments 1: " + argument1);
+        System.out.println("Arguments 2: " + argument2);
+        System.out.println("Arguments 3: " + argument3);
         System.out.println();
     }
 
